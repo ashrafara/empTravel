34b40c94f6ff6e4e1a623cf3e4d2cb747c2eed4d
Ash <aaalarayed@gmail.com>
2021-12-25 19:17:09 +0200

    changed select height

--- src/main/java/com/mycompany/myapp/repository/DecreeRepository.java
+++ src/main/java/com/mycompany/myapp/repository/DecreeRepository.java
@@ -5,7 +5,8 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -25,4 +26,36 @@ public interface DecreeRepository extends JpaRepository<Decree, Long> {
 
     @Query("select decree from Decree decree left join fetch decree.employees where decree.id =:id")
     Optional<Decree> findOneWithEagerRelationships(@Param("id") Long id);
+
+    @Query(
+        nativeQuery = true,
+        value = "select decree.decreenum, decree.decreeyear, decree.countrty, decree.dectype, decree.daynum, employee.name\n" +
+        "from decree, rel_decree__employee, employee\n" +
+        "WHERE decree.id = rel_decree__employee.decree_id\n" +
+        "and employee.id = rel_decree__employee.employee_id\n" +
+        "order by employee.name"
+    )
+    List<Decree> findAllreport();
+
+    @Query(
+        nativeQuery = true,
+        value = "select sum(decree.daynum) as dayCount, employee.name\n" +
+        "from decree, rel_decree__employee, employee\n" +
+        "WHERE decree.id = rel_decree__employee.decree_id\n" +
+        "and employee.id = rel_decree__employee.employee_id\n" +
+        "group by employee.name\n" +
+        "order by dayCount desc"
+    )
+    List<Decree> findAllDayCount();
+
+    @Query(
+        nativeQuery = true,
+        value = "select Count(decree.id) as decreeCount, employee.name \n" +
+        "from decree, rel_decree__employee, employee \n" +
+        "WHERE decree.id = rel_decree__employee.decree_id \n" +
+        "and employee.id = rel_decree__employee.employee_id \n" +
+        "group by employee.name \n" +
+        "order by decreeCount desc "
+    )
+    List<Decree> findAllDecreecount();
 }
